6b957c91b3f1e0b4ff31c9fe5df585fdd8ddebe0
Kim Zugic <kim.zugic@live.com>
2025-12-08 20:53:27 +0100

    changed charge button to show item count after second amount added

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -1,8 +1,7 @@
 import 'package:faida_pos/widgets/checkout_widgets/checkout_button_widget.dart';
 import 'package:faida_pos/widgets/checkout_widgets/favorites_widget.dart';
 import 'package:faida_pos/widgets/checkout_widgets/inventory_widget.dart';
-import 'package:faida_pos/widgets/checkout_widgets/numpad_display_widget.dart';
-import 'package:faida_pos/widgets/checkout_widgets/numpad_widget.dart';
+import 'package:faida_pos/widgets/checkout_widgets/numpad_tab_widget.dart';
 import 'package:faida_pos/widgets/shared/tabs_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -15,27 +14,32 @@ class Checkout extends StatefulWidget {
 }
 
 class _CheckoutState extends State<Checkout> {
-  String value = "";
+  String input = "";
   String partValues = "";
   double storedValue = 0;
   bool plusPressed = false;
   int _currentIndex = 0;
+  String currentSale = "";
+
 
   late final tabs = [
-    () => numpadTab(),
+    () => NumpadTabWidget(onNumPressed: onNumPressed,
+        onClear: onClear,
+        onPlusPressed: onPlusPressed,
+        value: input,
+        partValues: partValues,
+        storedValue: storedValue),
     () => InventoryWidget(),
     () => FavoritesWidget(),
   ];
 
-
-
   void onNumPressed(String digit){
-    setState (() => value += digit);
+    setState (() => input += digit);
   }
 
   void onClear() {
     setState(() {
-      value = "";
+      input = "";
       storedValue = 0;
       plusPressed = false;
       partValues = "";
@@ -44,16 +48,24 @@ class _CheckoutState extends State<Checkout> {
 
   void onPlusPressed(){
     setState(() {
-      final current = double.parse(value);
+      final current = double.parse(input);
       storedValue += current;
-      partValues += "$value + ";
-      value = "";
+      partValues += "$input + ";
+      input = "";
       plusPressed = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+
+    if(partValues.split('+').length - 1 > 1){
+      int count = '+'.allMatches(partValues).length;
+      currentSale = "Review $count items";
+    } else {
+      currentSale = "Charge: $storedValue TZS";
+    }
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -68,7 +80,7 @@ class _CheckoutState extends State<Checkout> {
             Align(alignment: Alignment.bottomCenter,
               child:
             CheckoutButtonWidget(label:
-            "Charge: $storedValue TZS",
+            currentSale,
                 onClicked: () {
               showModalBottomSheet<void>(
                   context: context,
@@ -80,7 +92,7 @@ class _CheckoutState extends State<Checkout> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Align(alignment: Alignment.topLeft,
-                            child: closeButton(),
+                            child: CloseButton(),
                           )
                         ],
                       ),
@@ -93,25 +105,5 @@ class _CheckoutState extends State<Checkout> {
       ),
     );
   }
-
-
-  Widget closeButton() =>
-    IconButton(
-        onPressed: () => Navigator.pop(context),
-        icon: Icon(Icons.close));
-
-  Widget numpadTab() =>
-      Column(
-        children: [
-          NumpadDisplayWidget(value: value),
-          Text(partValues),
-          Text("Total: $storedValue TZS"),
-          Expanded(
-              child: Numpad(
-                  onNumberPressed: onNumPressed,
-                  onClearPressed: onClear,
-                  onPlusPressed: onPlusPressed)),
-        ],
-      );
 }
 
---
+++ lib/widgets/checkout_widgets/numpad_tab_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'numpad_display_widget.dart';
+import 'numpad_widget.dart';
+
+class NumpadTabWidget extends StatelessWidget {
+
+  final Function(String) onNumPressed;
+  final VoidCallback onClear;
+  final VoidCallback onPlusPressed;
+  final String value;
+  final String partValues;
+  final double storedValue;
+
+  const NumpadTabWidget({
+    super.key,
+    required this.onNumPressed,
+    required this.onClear,
+    required this.onPlusPressed,
+    required this.value,
+    required this.partValues,
+    required this.storedValue});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        NumpadDisplayWidget(value: value),
+        Text(partValues),
+        Text("Total: $storedValue TZS"),
+        Expanded(
+            child: Numpad(
+                onNumberPressed: onNumPressed,
+                onClearPressed: onClear,
+                onPlusPressed: onPlusPressed)),
+      ],
+    );
+  }
+}
