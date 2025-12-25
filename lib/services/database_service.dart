c8b0146858e25cfa9373f5db2320cd901a07bb92
Kim Zugic <kim.zugic@live.com>
2025-12-25 10:52:42 +0100

    code clean up

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:faida_pos/models/product.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
