8d2025bfee55fd98e5fdacd796c49a59bbc9029c
Kim Zugic <kim.zugic@live.com>
2026-01-21 14:07:14 +0100

    setup to show transactions on today widget

--- lib/widgets/transaction_widgets/today_widget.dart
+++ lib/widgets/transaction_widgets/today_widget.dart
@@ -2,8 +2,22 @@ import 'package:flutter/material.dart';
 class TodayWidget extends StatelessWidget {
   const TodayWidget({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: SafeArea(child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+
+          ],
+        ),
+      )),
+    );
   }
 }
