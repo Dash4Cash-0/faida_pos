3323a520330da82d3b6b1ad00d30d4990c8e1273
Kim Zugic <kim.zugic@live.com>
2026-01-22 19:54:45 +0100

    added date to todays transactions

--- lib/widgets/transaction_widgets/today_widget.dart
+++ lib/widgets/transaction_widgets/today_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 class TodayWidget extends StatelessWidget {
   const TodayWidget({super.key});
 
@@ -6,17 +7,24 @@ class TodayWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final locale = Localizations.localeOf(context).languageCode;
+    DateTime now = DateTime.now();
+    String dateFormat = DateFormat('EEEE, d MMMM yyyy', locale).format(now);
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: SafeArea(child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            
+            SizedBox(height: 20),
+            Center(
+              child: Text(dateFormat,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headlineSmall)
+            )
 
           ],
-        ),
       )),
     );
   }
