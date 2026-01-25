80329e1a6bd3b9fc6990474779cafac88d0b9039
Kim Zugic <kim.zugic@live.com>
2026-01-25 12:09:14 +0100

    added dispose of controller on home root

--- lib/home_root.dart
+++ lib/home_root.dart
@@ -33,6 +33,12 @@ class _HomeRootState extends State<HomeRoot> {
   ];
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentIndex],
