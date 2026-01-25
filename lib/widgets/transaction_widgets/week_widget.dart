fc5cfc3a91386b2a110688cbbc6414fd12db6076
Kim Zugic <kim.zugic@live.com>
2026-01-25 19:41:04 +0100

    added week number

--- lib/widgets/transaction_widgets/week_widget.dart
+++ lib/widgets/transaction_widgets/week_widget.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class WeekWidget extends StatelessWidget {
   const WeekWidget({super.key});
 
+
+  int weekNumber(DateTime date) {
+    final firstDayOfYear = DateTime(date.year, 1, 1);
+    final daysSinceStart = date.difference(firstDayOfYear).inDays;
+    return ((daysSinceStart + firstDayOfYear.weekday) / 7).ceil();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    final getWeek = DateTime.now().day;
+    final currentWeek = weekNumber(DateTime.now());
+    DateTime currentDay = DateTime.now();
+    String dateFormat = DateFormat('EEEE').format(currentDay);
     return SafeArea(child: Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         SizedBox(height: 20),
         Center(child:
-        Text("Week: "))
+        Text("Week: $currentWeek",
+          style: Theme.of(context).textTheme.headlineSmall,))
       ],
     ));
   }
