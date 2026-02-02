b173831d954350bbf5478783e4b989b084353912
Kim Zugic <kim.zugic@live.com>
2026-02-02 15:11:56 +0100

    added buttons to this week with days and dates

--- lib/widgets/transaction_widgets/week_widget.dart
+++ lib/widgets/transaction_widgets/week_widget.dart
@@ -1,19 +1,43 @@
 import 'package:faida_pos/utils/transaction_utils/week_dates.dart';
+import 'package:faida_pos/widgets/menu_widgets/menu_button.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class WeekWidget extends StatelessWidget {
   const WeekWidget({super.key});
 
+
+  void onClickedDay() {
+
+}
+
   @override
   Widget build(BuildContext context) {
     final currentWeek = WeekDates().weekNumber(DateTime.now());
+    final locale = Localizations.localeOf(context).languageCode;
     return SafeArea(child: Column(
+
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         SizedBox(height: 20),
         Center(child:
         Text("Week: $currentWeek",
-          style: Theme.of(context).textTheme.headlineSmall,))
+          style: Theme.of(context).textTheme.headlineSmall,)),
+        SizedBox(height: 20),
+        Expanded(child:ListView
+          (children: List.generate(7, (index) {
+          final date = WeekDates().getWeekDates(DateTime.now())[index];
+          final day = DateFormat("EEEE", locale).format(date);
+
+          return MenuButton(label: "$day ${date.day}/${date.month}\tTotal: 5"
+              , onClicked: () => onClickedDay());
+        })
+        )
+    ),
+        SizedBox(height: 18),
+        Text("Sales this week: 10", style:
+        TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
+
       ],
     ));
   }
---
+++ lib/widgets/transaction_widgets/weekday_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class WeekdayButton extends StatelessWidget {
+
+  final String label;
+  final VoidCallback onClicked;
+
+  const WeekdayButton({super.key,
+    required this.label,
+    required this.onClicked});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Divider(height: 10, thickness: 2, indent: 0,endIndent: 0),
+            SizedBox(width: double.infinity,
+                child:
+                TextButton(
+                    onPressed: onClicked, style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 16)
+                ),
+                    child: Text(label, style: TextStyle(fontSize: 18),))),
+            Divider(height: 10, thickness: 2, indent: 0,endIndent: 0),
+          ],
+        )
+    );
+  }
+}
